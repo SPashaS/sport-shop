5fbfaad8754b6eed61c3b0fdfcce3ff2ec1b07bc
SPashaS <SPavel.LD@gmail.com>
2022-04-12 22:54:41 +0300

    page-menu

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -17,3 +17,27 @@ contactsInit();
 
 VK.Widgets.Group("vk_groups1", {mode: 0, width: "270px", height: "235"}, 1);
 VK.Widgets.Group("vk_groups2", {mode: 0, width: "270px", height: "235"}, 1);
+
+
+function pageMenuInit() {
+	let pageIconMenu = document.querySelector(".menu-page__burger");
+	let pageMenuBody = document.querySelector(".menu-page__body");
+	if (pageIconMenu) {
+		pageIconMenu.addEventListener("click", function (e) {
+			pageIconMenu.classList.toggle("_active");
+			pageMenuBody.classList.toggle("_active");
+		});
+	};
+}
+pageMenuInit();
+
+function subMenuInit() {
+	let subMenuParents = document.querySelectorAll('.menu-page__item_parent');
+	for (let index = 0; index < subMenuParents.length; index++) {
+		const subMenuParent = subMenuParents[index];
+		subMenuParent.addEventListener('click', function (e) {
+			subMenuParent.classList.toggle('_active-sub');
+		})
+	}
+}
+subMenuInit()
