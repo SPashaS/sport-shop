99a684550849eaa45fb93edfc5fe030e7aeea0b1
SPashaS <SPavel.LD@gmail.com>
2022-04-13 19:34:13 +0300

    search-page js

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -44,11 +44,27 @@ subMenuInit();
 
 
 
-function searchSelect() {
 	let searchValue = document.querySelector('.search-page__value');
 	// let categoriesSearch = document.querySelector('.categories-search-page');
 	searchValue.addEventListener("click", function (e) {
 		searchValue.classList.toggle('_active');
 	});
-}
-searchSelect();
+
+	let checkboxCategories = document.querySelectorAll('.categories-search-page__checkbox');
+	for (let index = 0; index < checkboxCategories.length; index++) {
+		const checkboxCategory = checkboxCategories[index];
+		checkboxCategory.addEventListener('change', function (e) {
+			checkboxCategory.classList.toggle('_active');
+
+			let checkboxActiveCategories = document.querySelectorAll('.categories-search-page__checkbox._active');
+			if (checkboxActiveCategories.length > 0) {
+				searchValue.classList.add('_categories');
+
+				let searchQuantity = searchValue.querySelector('.search-page__quantity');
+				searchQuantity.innerHTML = searchQuantity.getAttribute('data-text') + " " + checkboxActiveCategories.length;
+			} else {
+				searchValue.classList.remove('_categories');
+			}
+
+		})
+	}
