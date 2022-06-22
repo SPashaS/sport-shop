836c52f1706d438fd6c1b41545aad09f3aefa27c
SPashaS <SPavel.LD@gmail.com>
2022-06-22 21:58:04 +0300

    brands block

--- src/js/files/sliders.js
+++ src/js/files/sliders.js
@@ -176,6 +176,68 @@ function initSliders() {
 		});
 
 	}
+
+	if (document.querySelector('.brands__slider')) {
+		new Swiper('.brands__slider', {
+			// Подключаем модули слайдера
+			// для конкретного случая
+			//modules: [Navigation, Pagination],
+			modules: [Navigation],
+			/*
+			effect: 'fade',
+			autoplay: {
+				delay: 3000,
+				disableOnInteraction: false,
+			},
+			*/
+			observer: true,
+			observeParents: true,
+			slidesPerView: 5,
+			spaceBetween: 0,
+			// autoHeight: true,
+			speed: 800,
+			//touchRatio: 0,
+			//simulateTouch: false,
+			loop: true,
+			//preloadImages: false,
+			//lazy: true,
+			// Dotts
+			// pagination: {
+			// 	el: '.main-slider__dotts',
+			// 	clickable: true,
+			// },
+			// Arrows
+			navigation: {
+				nextEl: '.brands__slider-arrow_next',
+				prevEl: '.brands__slider-arrow_prev',
+			},
+			/*
+			breakpoints: {
+				320: {
+					slidesPerView: 1,
+					spaceBetween: 0,
+					autoHeight: true,
+				},
+				768: {
+					slidesPerView: 2,
+					spaceBetween: 20,
+				},
+				992: {
+					slidesPerView: 3,
+					spaceBetween: 20,
+				},
+				1268: {
+					slidesPerView: 4,
+					spaceBetween: 30,
+				},
+			},
+			*/
+			on: {
+
+			}
+		});
+
+	}
 }
 // Скролл на базе слайдера (по классу swiper_scroll для оболочки слайдера)
 function initSlidersScroll() {
